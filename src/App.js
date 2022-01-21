4b55af47f92e816adf3d829036869fc4bdd08dd9
Zijinhui <79233742+Zijinhui@users.noreply.github.com>
2022-01-21 16:20:35 -0500

    Unfixed router render issue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import AccountBalance from './components/AccountBalance'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home accountBalance={10}/>} />
+          <Route path="/AccountBalance" element={<AccountBalance />} />
+
+    
+        </Routes>     
+      </Router> 
+    )  
 }
 
-export default App;
+export default App
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import AccountBalance from './AccountBalance'
+//import {Link} from 'react-router-dom'
+
+function Home() {
+return (
+        <div>
+            {/*<img src="https://letstalkpayments.com/wp-content/uploads/2016/04/Bank.png" alt="bank" />*/}
+            <h1>Bank of React</h1>
+
+            
+           
+        </div>
+    )
+}
+export default Home
